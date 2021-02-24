feb33b0c655d78a7d52c2c861d5a99dde99ee8ef
Alexander Rodionov <alx1dr@gmail.com>
2021-02-24 20:10:35 +0300

    _

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-import numpy as np
-assert True
+import os
+
+# SUBMIT_ARGS = "--packages eisber:sarplus:0.2.6 pyspark-shell"
+# os.environ["PYSPARK_SUBMIT_ARGS"] = SUBMIT_ARGS
+
+from pyspark.sql import SparkSession
+
+spark = (
+    SparkSession.builder.appName("sample")
+    .master("local[*]")
+    .config("memory", "1G")
+    .config("spark.sql.shuffle.partitions", "1")
+    .config("spark.sql.crossJoin.enabled", True)
+    .config("spark.ui.enabled", False)
+    .getOrCreate()
+)
+
+print(spark.sparkContext.range(256).collect())
